c18e0a1838aaf27205d251f195c02dce5009514c
Nancy Hu <hunancymail@gmail.com>
2024-03-13 14:54:53 -0400

    delete the unused schedules folder

--- OneOnOne/schedules/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Schedule
-
-class ScheduleSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Schedule
-        fields = ['id', 'user', 'start_time', 'end_time'] 
--- OneOnOne/schedules/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import ScheduleListCreateView, ScheduleRetrieveUpdateDestroyView
-
-urlpatterns = [
-    path('schedules/', ScheduleListCreateView.as_view(), name='schedule-list-create'),
-    path('schedules/<int:pk>/', ScheduleRetrieveUpdateDestroyView.as_view(), name='schedule-retrieve-update-destroy'),
-]
