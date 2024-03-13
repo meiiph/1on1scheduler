93f7b6f1be19f5f256884d6d5b5e53989fb7b91c
Irene <irenehhuynh2@gmail.com>
2024-03-13 13:04:58 -0400

    Made contacts views

--- OneOnOne/contacts/models.py
+++ OneOnOne/contacts/models.py
@@ -3,6 +3,7 @@ from django.conf import settings
 from django.contrib.auth.models import User
 
 class Contact(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    curr_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='current_user')
+    contact_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='contact_user', verbose_name="username")
     name = models.CharField(max_length=100)
     email = models.EmailField()
--- OneOnOne/invitations/serializers.py
+++
@@ -1,8 +0,0 @@
-# from rest_framework import serializers
-# from .models import Invitation
-
-# class InvitationSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Invitation
-#         fields = ['id', 'sender', 'recipient', 'calendar', 'meeting_datetime', 'is_accepted']
-
